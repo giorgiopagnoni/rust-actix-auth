59b7eed5625e0484a3e4ee88daa3862884e1f00f
Giorgio Pagnoni <giorgio.pagnoni@sintattica.it>
2019-07-28 14:00:04 +0200

    domain and secret

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use dotenv;
 use actix_web::{HttpServer, App, web, HttpResponse, guard, middleware};
 use rust_auth::controllers::user_register;
 use rust_auth::dataservice::{MysqlR2D2DataService};
+use actix_identity::{IdentityService, CookieIdentityPolicy};
 
 fn main() {
     dotenv::dotenv().ok();
 
+    // domain
+    let domain = std::env::var("DOMAIN").unwrap_or("localhost".to_string());
     // socket
     let http_addr = std::env::var("HTTP_ADDR").expect("HTTP_ADDR must be set");
-
+    // secret
+    let secret = std::env::var("SECRET_KEY").expect("SECRET_KEY must be set");
     // data service
     let ds = MysqlR2D2DataService::new();
 
